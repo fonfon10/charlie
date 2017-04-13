7e9c466c5809280fd31acdc24485dae7a31eac77
Serge Lafontaine <slafontaine10@gmail.com>
2017-04-13 13:44:55 -0400

    modified producion.rb to dispaly backgrouyd pic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # added to display background picture
+  config.serve_static_assets = true
+  config.assets.compile = true
+
+
+
+
 end
